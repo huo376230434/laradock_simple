d3632cb97d31f29c4fb42a32e072cf39081bf314
huo <376230434@qq.com>
2019-12-25 17:54:30 +0800

    更改配置路径

--- php-worker/startup.sh
+++ php-worker/startup.sh
@@ -5,7 +5,7 @@
 php -r '$path="/var/www";var_dump($path); $handler = opendir($path); $arr = [];$crontab_path = "/etc/crontabs/root";\
             while( ($filename = readdir($handler)) !== false ) {\
                 if($filename != "." && $filename != ".." && is_dir($path . "/" . $filename) && file_exists($path . "/" . $filename."/artisan")){\
-                    $command_str = "* * * * *  php /var/www/$filename/artisan schedule:run >> /dev/null 2>&1".PHP_EOL;\
+                    $command_str = "* * * * *  php /var/www/$filename/$filename/artisan schedule:run >> /dev/null 2>&1".PHP_EOL;\
                     if (!strstr(file_get_contents($crontab_path), $command_str)) file_put_contents($crontab_path, $command_str, FILE_APPEND);\
                 } }'
 
